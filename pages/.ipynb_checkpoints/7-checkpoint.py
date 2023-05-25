397a7c9e04f0fef83583c7b280eb0024e49b37de
jdssoliman <jdssoliman@gmail.com>
2023-05-25 12:32:23 +0800

    1st commit jed

--- pages/.ipynb_checkpoints/7-checkpoint.py
+++ pages/.ipynb_checkpoints/7-checkpoint.py
@@ -1,9 +1,54 @@
 import streamlit as st
-
+from PIL import Image
 st.set_page_config(
     page_title="AI Regulation"
 )
 
 #### you may edit here onwards ###
 
-st.title("7")
+st.title("Meet the Team")
+
+st.write("Our name JAPGJae is an abbreviation of the first letters of the names of the team members, namely, Jamie, Ace, Pau, Gian and Jed. They found a mutual love for the Korean dish _japchae_ (glass noodles) and _jigae_ (hot stew), explaining the remaining two letters _ae_. ")
+
+#1
+st.subheader('Jamie Cuadra')
+
+st.write("Jamie Description ")
+
+image1 = Image.open('samaltman.webp') 
+st.image(image1, caption='Jamie Cuadra', use_column_width=True)
+
+#2
+st.subheader('Ace Canacan')
+
+st.write("Ace Description ")
+
+image2 = Image.open('ace.jpg') 
+st.image(image2, caption='Ace Canacan', use_column_width=True)
+
+#3
+st.subheader('Pau Sanchez')
+
+st.write("Pau Description ")
+
+image3 = Image.open('pau.jpg') 
+st.image(image3, caption='Pau Sanchez', use_column_width=True)
+
+#4
+st.subheader('Gian Servanez')
+
+st.write("Gian Description ")
+
+image4 = Image.open('samaltman.webp') 
+st.image(image4, caption='Gian Servanez', use_column_width=True)
+
+#5
+st.subheader('Jed Soliman')
+
+st.write("Jed Description ")
+
+image5 = Image.open('jed.jpg') 
+st.image(image5, caption='Jed Soliman', use_column_width=True)
+
+
+st.write("Feel free to add us on our socials! See you out there!")
