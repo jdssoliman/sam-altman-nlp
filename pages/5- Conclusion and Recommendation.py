a353527142219a740538b5be23e870b2f0c6217c
jtsanchez <127110855+jtsanchez@users.noreply.github.com>
2023-05-25 17:48:07 +0800

    Update 5- Conclusion and Recommendation.py

--- pages/5- Conclusion and Recommendation.py
+++ pages/5- Conclusion and Recommendation.py
@@ -4,4 +4,32 @@ st.set_page_config(
     page_title="AI Regulation"
 )
 
+st.title("Conclusion and Recommendation")
+
+st.markdown("By implementing these recommendations, lawmakers can strike a balance between harnessing the immense potential of AI and addressing the concerns surrounding its deployment. This comprehensive approach will lay the foundation for a responsible and sustainable AI ecosystem that benefits society as a whole.")
+
+
+st.header('AI & Systems: Global Regulation')
+st.markdown("It is imperative that lawmakers thoroughly examine the necessity of AI regulation and its global implementation. To do so effectively, We suggest allocating substantial funding and resources to support AI research and development initiatives. Furthermore, it is crucial to prioritize the education of policymakers about AI, equipping them with the necessary knowledge to make informed decisions regarding its regulation.")
+
+st.header('AI & People: Education & Upskilling')
+st.markdown("""
+Lawmakers should prioritize policies that foster the growth and development of AI through targeted education and skills development programs. By implementing comprehensive AI education initiatives, policymakers can ensure that individuals are well-prepared to embrace AI's potential. Additionally, initiating educational campaigns will be instrumental in increasing public awareness about the benefits of AI, dispelling misconceptions, and garnering support for its responsible use.
+""")
+
+st.header('AI & Technology: Ethics')
+st.markdown("""
+To build trust and ensure the ethical use of AI, it is essential to regulate the development of AI systems, promoting transparency and fairness. We recommend establishing clear guidelines and regulations that outline ethical practices in AI development and deployment.
+""")
+
+st.header("AI & Innovation: Public Consultation")
+st.markdown("""
+Lawmakers have a critical role in supporting AI-driven innovation. By actively facilitating innovation forums and public discussions, policymakers can effectively demonstrate the profound impact of AI on driving innovation across various sectors. Encouraging collaboration and knowledge sharing among researchers, developers, and industry leaders will create an ecosystem conducive to transformative advancements in AI.
+""")
+            
+st.header("AI & Industry: Inclusivity of SMEs")
+st.markdown("""
+Policymakers should prioritize the inclusivity of small and medium enterprises (SMEs) in the AI landscape. Promoting AI adoption among SMEs will not only foster economic growth but also ensure a level playing field. We recommend strategizing measures to mitigate job displacements, such as providing comprehensive support programs and workshops for upskilling and facilitating smooth job transitions. Exploring options like unemployment benefits can also help alleviate the immediate impact of job loss and ensure social stability.
+""")
+
 #### you may edit here onwards ###
